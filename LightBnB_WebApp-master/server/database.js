bead1f765adf185f170b6d77d89c995ece17b795
Ysabel Garcia <ysabel.garcia@live.com>
2021-09-17 02:48:34 +0000

    create a new listing

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -172,23 +172,8 @@ const getAllProperties = function(options, limit = 10) {
 
   // 6
   return pool.query(queryString, queryParams).then((res) => res.rows);
-
-
-  pool.query(
-    `SELECT * FROM properties LIMIT $1`,
-    [limit])
-    .then((result) => {
-      return (result.rows)})
-      .catch((err) => {
-        return (err.message)});
-        
-        const limitedProperties = {};
-        for (let i = 1; i <= limit; i++) {
-          limitedProperties[i] = properties[i];
-        }
-        return Promise.resolve(limitedProperties);
 }
-      exports.getAllProperties = getAllProperties;
+exports.getAllProperties = getAllProperties;
 
 
 /**
@@ -197,9 +182,16 @@ const getAllProperties = function(options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  return pool.query(`
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+    `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+  .then((result) => {
+    return Promise.resolve(result.rows[0])})
+  .catch((err) => {
+    return (err.message)
+  });
 }
 exports.addProperty = addProperty;
